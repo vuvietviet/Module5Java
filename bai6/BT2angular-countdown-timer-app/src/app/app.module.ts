b706c0bd634e4850d898c61fd3eadd2fa895068a
vu viet <vvviet97@gmail.com>
2022-08-19 17:39:19 +0700

    BT2Bai6

---
+++ bai6/BT2angular-countdown-timer-app/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CountdownTimerComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
