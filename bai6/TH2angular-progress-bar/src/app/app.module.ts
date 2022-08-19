faee424ae8dff8adce86eb047347d32d12011a08
vu viet <vvviet97@gmail.com>
2022-08-19 12:04:59 +0700

    TH2Bai6

---
+++ bai6/TH2angular-progress-bar/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProgressBarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
